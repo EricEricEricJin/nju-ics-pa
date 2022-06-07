ac4f30ee4b8f9767f79a16f059e3c2f22c6687c6
tracer-ics2021 <tracer@njuics.org>
2022-06-07 13:59:14 +0800

    >  compile 114514191  Static hostname: eric-MacBookPro        Icon name: computer-laptop          Chassis: laptop       Machine ID: c9f2c76efa1f4cb696e55ca4fa3b5d55          Boot ID: 1178564a334c46af8f86a1df9d275b6e Operating System: Ubuntu 22.04 LTS           Kernel: Linux 5.15.0-35-generic     Architecture: x86-64  Hardware Vendor: Apple Inc.   Hardware Model: MacBookPro8,1  13:59:14 up 13:14,  1 user,  load average: 3.73, 3.01, 2.39

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -1,4 +1,6 @@
 #include <isa.h>
+#include <stdio.h>
+#include "common.h"
 #include "local-include/reg.h"
 
 const char *regs[] = {
@@ -9,6 +11,9 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+  for (int i = 0; i < sizeof(regs) / sizeof(char*); i++) {
+    printf("%s  %x \n", reg_name(i, sizeof(rtlreg_t)), gpr(i));
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
